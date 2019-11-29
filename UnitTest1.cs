fda5bdb906cda5e611d1cabafd98c8be81f7fe19
marcelinoSousa <marcelino.feliciano@hotmail.com>
2019-11-28 20:29:46 -0400

    Prova Final - Avaliação Git

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+/* 
+   Avaliação Oficial
+   Em 28/11/2019
+   Marcelino Feliciano de Sousa 
+ */
+namespace Prova2Bimestre
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
